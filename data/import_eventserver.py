4c555f4e6296eb0ff10ee614cc77675348305eef
detrevid <detrevid@gmail.com>
2015-03-24 04:56:13 +0200

    Switch to small dataset

--- data/import_eventserver.py
+++ data/import_eventserver.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     description="Import sample data for classification engine")
   parser.add_argument('--access_key', default='invald_access_key')
   parser.add_argument('--url', default="http://localhost:7070")
-  parser.add_argument('--file', default="./data/data.txt")
+  parser.add_argument('--file', default="./data/small_data.txt")
 
   args = parser.parse_args()
   print args
