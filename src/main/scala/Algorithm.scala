2e8c833f8482145ebbcd9038dc05edb86ccaef6e
detrevid <detrevid@gmail.com>
2015-04-05 00:25:22 +0300

    Change Network tuning

--- src/main/scala/Algorithm.scala
+++ src/main/scala/Algorithm.scala
@@ -13,15 +13,16 @@ import org.deeplearning4j.nn.conf.`override`.ClassifierOverride
 import org.deeplearning4j.nn.layers.factory.PretrainLayerFactory
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.lossfunctions.LossFunctions
 
 case class AlgorithmParams(
-                            iterations: Int    = 10,
-                            layers:     Int    = 2,
-                            momentum:   Double = 0.9,
-                            dropOut:    Double = 0.8
-                            ) extends Params
+  iterations: Int = 10,
+  layers: Int = 2,
+  momentum: Double = 0.9,
+  dropOut: Double = 0.8
+) extends Params
 
 class Algorithm(val ap: AlgorithmParams)
   extends P2LAlgorithm[PreparedData, Model, Query, PredictedResult] {
@@ -30,18 +31,20 @@ class Algorithm(val ap: AlgorithmParams)
 
   def train(sc: SparkContext, data: PreparedData): Model = {
 
+    Nd4j.MAX_SLICES_TO_PRINT = -1
+    Nd4j.MAX_ELEMENTS_PER_SLICE = -1
     val conf: MultiLayerConfiguration =
       new NeuralNetConfiguration.Builder().iterations(ap.iterations)
-        .layerFactory(new PretrainLayerFactory(classOf[RBM])).weightInit(WeightInit.SIZE)
-        .dist(Nd4j.getDistributions.createNormal(1e-5, 1)).activationFunction("tanh")
+        .layerFactory(new PretrainLayerFactory(classOf[RBM])).weightInit(WeightInit.DISTRIBUTION)
+        .dist(Nd4j.getDistributions.createNormal(0, 1)).activationFunction("tanh")
         .momentum(ap.momentum).dropOut(ap.dropOut)
-        .optimizationAlgo(OptimizationAlgorithm.GRADIENT_DESCENT)
-        .constrainGradientToUnitNorm(true).k(5).regularization(true)
+        .optimizationAlgo(OptimizationAlgorithm.LBFGS)
+        .constrainGradientToUnitNorm(true).k(1).regularization(true)
         .l2(2e-4).visibleUnit(RBM.VisibleUnit.GAUSSIAN)
         .hiddenUnit(RBM.HiddenUnit.RECTIFIED)
-        .lossFunction(LossFunctions.LossFunction.RECONSTRUCTION_CROSSENTROPY)
-        .learningRate(1e-1f).nIn(3).nOut(data.labels.length).list(ap.layers)
-        .useDropConnect(false)
+        .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+        .learningRate(1e-1f).iterationListener(new ScoreIterationListener(2))
+        .nIn(3).nOut(data.labels.length).list(ap.layers)
         .hiddenLayerSizes(3).`override`(new ClassifierOverride(1)).build
 
     val d: MultiLayerNetwork = new MultiLayerNetwork(conf)
@@ -58,17 +61,15 @@ class Algorithm(val ap: AlgorithmParams)
 }
 
 class Model(
-             val labels: Array[String],
-             val net: MultiLayerNetwork)
-extends Serializable {
+    val labels: Array[String],
+    val net: MultiLayerNetwork)
+  extends Serializable {
 
   @transient lazy val logger = Logger[this.type]
-  
+
   def predict(features: Array[Double]): String = {
-    val features_array = Nd4j.create(Array(features,Array(3.0,0.0,0.0)))
-    this.logger.info(features_array.toString)
-    this.logger.info("AFTER FEATURES ARRAY")
-    val pred : Int = net.predict(features_array)(0)
+    val features_array = Nd4j.create(features)
+    val pred = net.predict(features_array)(0)
     labels(pred)
   }
 }
